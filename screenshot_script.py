0c624f4f24c9057be2c5722ffdd83a538fe9421c
rrrrrroman <roman.kharkovskoy@yandex.ru>
2022-04-05 20:50:49 +0300

    Создание папки для каждого юзера

--- screenshot_script.py
+++ screenshot_script.py
@@ -1,12 +1,11 @@
 from cv2 import cv2
 import os
 
-
-def take_screenshot():
-    cap = cv2.VideoCapture("video.mp4")#Принимает в качестве параметра адрес на видео
+def take_screenshot(qwerty):
+    cap = cv2.VideoCapture("cache/video.mp4")#Принимает в качестве параметра адрес на видео
     count = 0
-    if not os.path.exists("dataset_from_video"):#это нужно будет усовершенствовать и сделать папки для каждого профиля в данной директории
-        os.mkdir("dataset_from_video")
+    if not os.path.exists(f'{qwerty}'):#это нужно будет усовершенствовать и сделать папки для каждого профиля в данной директории
+        os.mkdir(f'{qwerty}')
 
     while True:
         ret, frame = cap.read()
@@ -19,14 +18,12 @@ def take_screenshot():
             cv2.waitKey(50)
 
             if frame_id % 10 == 0: #тут каждый десятый кадр скриншотится(позже сделать зависимость от fps)
-                cv2.imwrite(f"dataset_from_video/{count}.jpg", frame)
+                cv2.imwrite(f"{qwerty}/{count}.jpg", frame)
                 print(f"Сделал скриншот {count}")
                 count += 1 
         else:
-            print("Ошибка. Не могу сделать скриншот!")#в конце ошибка это нормально, потому что заканчивается видео
+            # print("Ошибка. Не могу сделать скриншот!")#в конце ошибка это нормально, потому что заканчивается видео
             break
 
     cap.release()
     cv2.destroyAllWindows()    
-
-take_screenshot()

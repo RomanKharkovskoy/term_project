1bed09c4cf909dad4cb623748c40e5055f2a1af8
rrrrrroman <roman.kharkovskoy@yandex.ru>
2022-04-16 19:50:19 +0300

    Add comparing function

--- bot.py
+++ bot.py
@@ -1,14 +1,15 @@
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ContentType
-from top_secret import token
 import logging
 import sqlite3
-from screenshot_script import take_screenshot
-import os
+import json
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ContentType
+
+from cmprg import cleaning, discr_compare, extracting_faces, save_src
+from top_secret import token
 
 bot = Bot(token = token)
 dp = Dispatcher(bot)
-count = 0
 
 logging.basicConfig(level=logging.INFO)
 
@@ -21,33 +22,48 @@ def db_table_val(username: str, user_id: int, photo_dir: str):
 
 @dp.message_handler(commands='start')
 async def start(message: types.Message):
-    await message.reply('Чтобы начать работу напиши мне "Привет"')
+    await message.reply('Привет дружище! Отправь свою фотографию, на которой хорошо видно лицо')
 
 async def goodby(message: types.Message):
     await message.reply('До скорых встреч')
 
 dp.register_message_handler(goodby, commands='by')
 
-@dp.message_handler(content_types=[ContentType.VIDEO_NOTE])
-async def vedio_machine(message: types.Message):
-    await message.answer('Скачиваю полученный файл')
-    destination = 'cache/video.mp4'
+@dp.message_handler(content_types=['photo'])
+async def add_photo(message: types.Message):
+    destination = f'ideal_images/{message.from_user.username}.jpg'
+    await message.photo[-1].download(destination_file=destination)
+    snames= extracting_faces(f'ideal_images/{message.from_user.username}.jpg', message.from_user.username)
     us_id = message.from_user.id
     username = message.from_user.username
-    photo_dir = f'{message.from_user.username}'
-    db_table_val(username=username, user_id=us_id, photo_dir=photo_dir)
-    await message.video_note.download(destination_file=destination)
-    await message.answer('Скачал. Ищи себя в прошмандовках Азербайджана)')
-    os.remove(f'{message.from_user.username}')
-    take_screenshot(message.from_user.username)
-    os.remove('cache/video.mp4')
+    db_table_val(username=username, user_id=us_id, photo_dir=snames)
+    await message.answer('Добавил тебя в нашу базу данных :) \n Теперь отправь пожалуйста мне кружок, в котором будет видно твоё милое лицо)')
 
-@dp.message_handler(content_types=['text'])
-async def get_text_messages(message: types.Message):
-    if message.text.lower() == "привет":
-        await message.answer('Привет дружище! Добавил тебя в нашу базу данных :) \n Теперь отправь пожалуйста мне кружок, в котором будет видно твоё милое лицо)')
-    
-    
+@dp.message_handler(content_types=[ContentType.VIDEO_NOTE])
+async def video_machine(message: types.Message):
+    await message.answer('⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️')
+    await bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id+1, text='🟧⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️')
+    await bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id+1, text='🟧🟧⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️')
+    await bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id+1, text='🟧🟧🟧⬛️⬛️⬛️⬛️⬛️⬛️⬛️')
+    await bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id+1, text='🟧🟧🟧🟧⬛️⬛️⬛️⬛️⬛️⬛️')
+    await bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id+1, text='🟧🟧🟧🟧🟧⬛️⬛️⬛️⬛️⬛️')
+    await bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id+1, text='🟧🟧🟧🟧🟧🟧⬛️⬛️⬛️⬛️')
+    await bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id+1, text='🟧🟧🟧🟧🟧🟧🟧⬛️⬛️⬛️')
+    await bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id+1, text='🟧🟧🟧🟧🟧🟧🟧🟧⬛️⬛️')
+    await bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id+1, text='🟧🟧🟧🟧🟧🟧🟧🟧🟧⬛️')
+    await bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id+1, text='🟧🟧🟧🟧🟧🟧🟧🟧🟧🟧')
+    destination = f'images_to_compare/temp_video.mp4'
+    await message.video_note.download(destination_file=destination)
+    save_src("temp_video")
+    await message.answer('Скачал твоё видео, сейчас сравню лицо с базой данных.')  
+    for row in cursor.execute('select photo_dir, username from users'):
+        print(row[1])
+        temp = json.loads(row[0])
+        if discr_compare(temp) == [True]:
+            user = row[1]
+            await message.answer(f'На данном видео @{user}')
+            cleaning()
+            break
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
